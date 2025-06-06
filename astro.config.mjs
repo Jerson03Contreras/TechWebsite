578025cf7c934fa6576bcad88e54218ba9d34a62
Jerson <jerson_anthon@hotmail.com>
2025-06-06 12:51:38 -0500

    Automatizar el index, mejorar la mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx'; // <-- Esta línea es crucial
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap'; // 1. Importar
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()], // <-- Y esta también es crucial
-  // ... otras configuraciones si las tienes
+  // 2. Definir la URL final de tu sitio
+  site: 'https://tech-website-pi.vercel.app/', // ¡Reemplaza con tu dominio!
+  integrations: [
+    mdx(),
+    sitemap() // 3. Añadir la integración
+  ],
 });
